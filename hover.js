49bfc20dc8c0cbf0c461ab0e265a4edbd35dfb51
vhr121 <veereshhr121@gmail.com>
2016-11-08 21:21:36 +0530

    added code

---
+++ hover.js
@@ -0,0 +1,16 @@
+
+		function mover()
+		{
+
+
+			if(num==0){
+				document.p[0].style.z-index=30;
+			}
+			else if(num==1){
+				document.p[1].style.z-index=30;
+			}
+			else{
+				document.p[2].style.z-index=30;
+			}
+	
+		}
